d1d913173b81ea2a053c82172c5e9d3b329a57f7
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-21 17:22:06 +0000

    Added decisions

--- classwork_1_IanBieg.py
+++ classwork_1_IanBieg.py
@@ -1,3 +1,11 @@
 #classwork Ian Bieg
-userAge = int(input('How old are you?'))
-drinkChoice = input('What do you want to drink?')
+userAge = int(input('How old are you?\n'))
+drinkChoice = input('What do you want to drink?\n')
+if userAge >= 21:
+    print('Welcome to grown-up drink place')
+    if drinkChoice.lower() == 'water':
+        print('It\'s important to stay hydrated')
+    else:
+        print('Enjoy your grown-up beverage')
+else:
+    print('You\'re not old enough to be here')
