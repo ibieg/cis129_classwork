af099a7ca1f72639f4783bf1ebbefe48faa948b3
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-21 17:28:48 +0000

    commit

--- classwork_1_IanBieg.py
+++ classwork_1_IanBieg.py
@@ -7,5 +7,8 @@ if userAge >= 21:
         print('It\'s important to stay hydrated')
     else:
         print('Enjoy your grown-up beverage')
+elif userAge < 21 and drinkChoice.lower() == 'water':
+    print('Okay fine now here\'s some free water scram!')
 else:
     print('You\'re not old enough to be here')
+print('Thanks for stopping by')
