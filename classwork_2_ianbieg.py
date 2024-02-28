4e8a0a6cb08fef818a12b5c96b8a25d174b09bff
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-28 16:43:21 +0000

    Added something

--- classwork_2_ianbieg.py
+++ classwork_2_ianbieg.py
@@ -1,1 +1,10 @@
-#anything
+#ian bieg
+age = int(input('Enter your age: '))
+if age >12 and age < 20:
+    print('You\'re a teen!')
+elif age > 70:
+    print('You\'re a senior')
+elif age <= 0 or age >= 100:
+    print('ERROR')
+else:
+    print('Hello')
