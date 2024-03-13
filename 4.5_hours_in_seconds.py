3d07da764fc28111928b5759a9ab1ea8c2f9d6a5
ibieg <158854639+ibieg@users.noreply.github.com>
2024-03-13 05:30:52 -0700

    Add files via upload

--- 4.5_hours_in_seconds.py
+++ 4.5_hours_in_seconds.py
@@ -1,5 +1,5 @@
-def seconds_since_midnight(seconds = int(input('How many seconds have passed: '))):
-    hour_in_seconds = (int(input('How many hours have passed: ')) * 60 ** 2)
-    minute_in_seconds = (int(input('How many minutes have passed: ')) * 60)
+def seconds_since_midnight(seconds = int(input('How many seconds have passed this minute: '))):
+    hour_in_seconds = (int(input('How many hours have passed today: ')) * 60 ** 2)
+    minute_in_seconds = (int(input('How many minutes have passed this hour: ')) * 60)
     return print('It\'s been',hour_in_seconds + minute_in_seconds + seconds,'Seconds since midnight')
 seconds_since_midnight()
